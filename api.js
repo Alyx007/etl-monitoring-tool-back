1a7b96a3f958a4fd801b44c071bb695d622f45eb
Aleksandra <aleksandra.stupiec@gmail.com>
2025-05-08 18:42:47 -0600

    initial commit

---
+++ api.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const dotenv =require('dotenv');
+const app = express();
+
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello, Express.js Server!</h1>');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
