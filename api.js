b6403f7cb5531d0621219821d490c660a2b567aa
Aleksandra <aleksandra.stupiec@gmail.com>
2025-06-17 13:22:58 -0600

    testing connection

--- api.js
+++ api.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const dotenv =require('dotenv');
-const homeRouter = require('./routes/home'); 
+//const homeRouter = require('./routes/home'); 
 
 dotenv.config();
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
-app.use('/', homeRouter);
+//app.use('/', homeRouter);
+
+app.get('/', (req, res) => {
+    res.send(`
+    <!DOCTYPE html>
+    <html>
+      <head><title>ETL Monitor</title></head>
+      <body>
+        <h1>Welcome to ETL Monitor!</h1>
+        <p>Server is running ✅</p>
+      </body>
+    </html>
+        `);
+});
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on: http://localhost:${port}`);
 });
